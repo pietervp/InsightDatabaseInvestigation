a9bdb5cf4f38c23a74d77a1b9e92ea727808664f
Pieter Van Parys <pieter.vanparys@gmail.com>
2013-11-26 12:01:48 +0100

    Initial commit

---
+++ src/InsightDatabaseInvestigation/Scripts/insertDummyData.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO UserGroup VALUES
+	(NULL, 'Group1', 'None'),
+	(NULL, 'Group2', 'None');
+
+[NEXT]
+
+INSERT INTO [User] VALUES
+	(NULL, 'Pieter', 'Van Parys', 'V', 'me@provider.com', '045877896', 'None'),
+	(NULL, 'Lucas', 'Van Parys', 'V', 'lucas@provider.com', '045877896', 'None'),
+	(NULL, 'Louise', 'Van Parys', 'V', 'louise@provider.com', '045877896', 'None'),
+	(NULL, 'Annelies', 'Van Parys', 'V', 'annelies@provider.com', '045877896', 'None');
+	
+[NEXT]
+	
+INSERT INTO Membership VALUES
+	(NULL, 1,1),
+	(NULL, 1,2),
+	(NULL, 1,3),
+	(NULL, 2,4),
+	(NULL, 2,1);
